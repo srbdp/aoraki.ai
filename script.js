d96437da8e1f5d4ae63568e0f9a37609b4e8b03c
Brandon Pizzacalla <bpizzacalla@gmail.com>
2023-05-06 11:52:04 +1200

    tweaks for better mobile experience 3

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ class Star {
         this.x = Math.random() * window.innerWidth;
         this.y = Math.random() * window.innerHeight;
         this.size = Math.random() * 4.8 + 0.2;
-        this.speedX = Math.random() * 3 - 1.5;
-        this.speedY = Math.random() * 3 - 1.5;
+        this.speedX = Math.random() * 0.5 - 0.25;
+        this.speedY = Math.random() * 0.5 - 0.25;
     }
 
     update() {
